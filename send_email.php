81979d9947016b1f4e21d95c3e4acf698f2b7c09
zirran <108167534+zirran@users.noreply.github.com>
2023-08-24 08:38:28 +0800

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+
+    $to = "16774996@qq.com";
+    $subject = "New Message from Contact Form";
+    $body = "名称: $name\n\n邮箱地址: $email\n\n信息内容: $message";
+
+
+    if (mail($to, $subject, $body)) {
+        echo "信息发送成功！感谢对我们的支持，等待我们的回复吧";
+    } else {
+        echo "对不起，信息发送失败了@~@";
+    }
+}
+?>
